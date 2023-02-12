4cf12ee5874920e15c376e254bfb8b3f82badd0f
d6ve <123152386+d6ve@users.noreply.github.com>
2023-02-12 07:49:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -22,14 +22,26 @@ def inference(model_inputs:dict) -> dict:
     if mp3BytesString == None:
         return {'message': "No input provided"}
     
+    inputLanguage = model_inputs.get('language', None)
+    if inputLanguage == None:
+        return {'message': "No language provided"}
+    
+    inputTask = model_inputs.get('task', None)
+    if inputTask == None:
+        return {'message': "No task provided"}
+        
+    args = dict(
+        language = (None if inputLanguage.lower() == "auto" else inputLanguage),
+        task = inputTask
+    )
+    
     mp3Bytes = BytesIO(base64.b64decode(mp3BytesString.encode("ISO-8859-1")))
     with open('input.mp3','wb') as file:
         file.write(mp3Bytes.getbuffer())
     
     # Run the model
-    result = model.transcribe("input.mp3", language='es')
-    # outputText = {"text":result["text"]}
-    # outputSegments = {"segments":result["segments"]}
+    result = model.transcribe("input.mp3",**args)
     os.remove("input.mp3")
+
     # Return the results as a dictionary
     return result
